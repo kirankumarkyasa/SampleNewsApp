cc79f060f8ee46f22748b7ece1a6007f81e951ed
INNOMINDSHYD\pkota <pkota@innominds.com>
2016-09-12 19:08:45 +0530

    Navigation drawer implementation

--- NYTNews/app/src/main/java/com/example/pkota/nytnews/Activities/MainActivity.java
+++ NYTNews/app/src/main/java/com/example/pkota/nytnews/Activities/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.pkota.nytnews.Activities;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.pkota.nytnews.R;
 import com.example.pkota.nytnews.retrofit.NewsAPI;
 import com.example.pkota.nytnews.retrofit.NewsApiInterface;
+import com.example.pkota.nytnews.utils.NavDrawerItem;
 import com.example.pkota.nytnews.utils.News;
 
 import java.util.ArrayList;
@@ -25,9 +36,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
 
-    private static RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private static RecyclerView recyclerView;
     private static ArrayList<News> data;
@@ -35,15 +45,29 @@ public class MainActivity extends AppCompatActivity {
     private static ArrayList<Integer> removedItems;
     private static final String TAG = "MainActivity";
 
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.recycler_fragment);
+        drawerFragment.setUp(R.id.recycler_fragment, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
 
-     //   myOnClickListener = new MyOnClickListener(this);
+        // display the first navigation drawer view on app launch
+        displayView(0);
 
+    }
+    public void setRecycler(Call<News> call) {
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
 
@@ -51,90 +75,54 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        NewsApiInterface apiService =
-                NewsAPI.getClient().create(NewsApiInterface.class);
-
-        Call<News> call = apiService.getAllNews();
         call.enqueue(new Callback<News>() {
             @Override
-            public void onResponse(Call<News>call, Response<News> response) {
-                Log.d(TAG, "heylo: " +response);
+            public void onResponse(Call<News> call, Response<News> response) {
+                Log.d(TAG, "heylo: " + response);
                 List<News> movies = response.body().getResults();
                 Log.d(TAG, "Number of movies received: " + movies.size());
                 recyclerView.setAdapter(new CustomList(movies));
             }
 
             @Override
-            public void onFailure(Call<News>call, Throwable t) {
+            public void onFailure(Call<News> call, Throwable t) {
                 Log.d(TAG, "Number of movies received: " + t.toString());
             }
         });
-
     }
 
-
- /*   private static class MyOnClickListener implements View.OnClickListener {
-
-        private final Context context;
-
-        private MyOnClickListener(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public void onClick(View v) {
-            removeItem(v);
-        }
-
-        private void removeItem(View v) {
-            int selectedItemPosition = recyclerView.getChildPosition(v);
-            RecyclerView.ViewHolder viewHolder
-                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
-            TextView textViewName
-                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
-            String selectedName = (String) textViewName.getText();
-            int selectedItemId = -1;
-            for (int i = 0; i < MyData.nameArray.length; i++) {
-                if (selectedName.equals(MyData.nameArray[i])) {
-                    selectedItemId = MyData.id_[i];
-                }
-            }
-            removedItems.add(selectedItemId);
-            data.remove(selectedItemPosition);
-            adapter.notifyItemRemoved(selectedItemPosition);
-        }
-    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.add_item) {
-            //check if any items to add
-            if (removedItems.size() != 0) {
-                addRemovedItemToList();
-            } else {
-                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
-            }
+    private void displayView(int position) {
+        NewsApiInterface apiService =
+                NewsAPI.getClient().create(NewsApiInterface.class);
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                setRecycler(apiService.getAllNews());
+                break;
+            case 1:
+                setRecycler(apiService.getSportsNews());
+                break;
+            case 2:
+                setRecycler(apiService.getHealthNews());
+                break;
+            case 3:
+                setRecycler(apiService.getBusinessNews());
+                break;
+            case 4:
+                setRecycler(apiService.getArtNews());
+                break;
+            default:
+                break;
         }
-        return true;
-    }
 
-    private void addRemovedItemToList() {
-        int addItemAtListPosition = 3;
-        data.add(addItemAtListPosition, new DataModel(
-                MyData.nameArray[removedItems.get(0)],
-                MyData.versionArray[removedItems.get(0)],
-                MyData.id_[removedItems.get(0)],
-                MyData.drawableArray[removedItems.get(0)]
-        ));
-        adapter.notifyItemInserted(addItemAtListPosition);
-        removedItems.remove(0);
-    }*/
+        getSupportActionBar().setTitle(title);
+        // set the toolbar title
+
+    }
 }
---
+++ NYTNews/app/src/main/java/com/example/pkota/nytnews/utils/NavDrawerItem.java
@@ -0,0 +1,49 @@
+package com.example.pkota.nytnews.utils;
+
+/**
+ * Created by pkota on 08-09-2016.
+ */
+public class NavDrawerItem {
+
+    private String title;
+    private int icon;
+    // boolean to set visiblity of the counter
+    private boolean isCounterVisible = false;
+
+    public NavDrawerItem(){}
+
+    public NavDrawerItem(String title, int icon){
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public NavDrawerItem(String title, int icon, boolean isCounterVisible, String count){
+        this.title = title;
+        this.icon = icon;
+        this.isCounterVisible = isCounterVisible;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getIcon(){
+        return this.icon;
+    }
+
+    public boolean getCounterVisibility(){
+        return this.isCounterVisible;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setIcon(int icon){
+        this.icon = icon;
+    }
+
+    public void setCounterVisibility(boolean isCounterVisible){
+        this.isCounterVisible = isCounterVisible;
+    }
+}
